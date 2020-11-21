d8f0ec0570b877f5641e95616169b3b99519a807
LAN69 <gexlandalex@gmail.com>
2020-11-21 22:20:09 +0300

    first init

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+from requests import Session
+import json
+from datetime import datetime
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': '4a9bbdcd-79ea-4420-b4e4-c177fa51227e',
+}
+session = Session()
+session.headers.update(headers)
+
+
+def test_access_to_10_tickers_sorted_by_volume():
+    parameters = {
+        'sort': 'volume_24h',
+        'limit': '10',
+    }
+    time_before_response = datetime.now().microsecond
+    response = session.get(url, params=parameters)
+    time_after_response = datetime.now().microsecond
+    data = json.loads(response.text)
+
+    assert time_after_response - time_before_response < 500000
+    assert str(datetime.now().date()) in data['status']['timestamp']
+    assert sys.getsizeof(data) < 10000
